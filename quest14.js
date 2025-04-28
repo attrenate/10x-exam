eb204de3138a910979e0f57ea0d48f675f46569f
attrenate <nikolozghomidzevol100@gmail.com>
2025-04-28 20:19:38 +0400

    10x-exam

---
+++ quest14.js
@@ -0,0 +1,7 @@
+// quest14.js
+
+const registry = ["nodeA", "nodeB", "nodeC"];
+registry.push("nodeBackup");
+const totalRegistry = registry.length;
+
+console.log(`Registry count: ${totalRegistry}`);
